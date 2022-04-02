619315d0be92009bf851ed4b32d4714e0f3c584f
Shatha Siala <80515211+ShathaCodes@users.noreply.github.com>
2022-04-02 10:22:45 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,5 +1,11 @@
 def buildApp() {
   echo 'building the application with groovy!'
 }
-
+def testApp() {
+  echo 'testing the application...'
+}
+def deployApp() {
+  echo 'deploying the application...'
+				echo "version is ${params.VERSION}"
+}
 return this
